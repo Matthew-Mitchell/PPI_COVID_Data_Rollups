c995904e1614358b7eec128d6031535d72ed2efe
Matthew-Mitchell <mmitchell17@protonmail.com>
2020-12-09 14:17:16 -0500

    Fixing possible duplicates error between most recent day and first Mondays in pre_pivot df. Updating Scrape_Date formatting (changing to strict date from timestamp). Improving formatting of excel file by specifying column widths to be displayed.

--- COVID_Prison_Transformations.py
+++ COVID_Prison_Transformations.py
@@ -3,7 +3,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import datetime
 
-
+#Read in NYU Data
 df = pd.read_csv('https://raw.githubusercontent.com/publicsafetylab/public-psl-jdi-pops/master/data.csv')
 
 
@@ -19,7 +19,7 @@ after_mar10 = len(df)
 print("{} post march 10th.".format(after_mar10/orig_len))
 
 
-# ## Subset to Jails with >75% days populated with data
+## Subset to Jails with >75% days populated with data
 
 
 
@@ -33,7 +33,7 @@ over_75percent_data = len(df)
 
 
 
-# ### Quantifying Data Drop-Off
+## Quantifying Data Drop-Off
 
 
 percent = str(round(len(jails_to_include)/len(ndata_per_jail),2)*100)
@@ -41,16 +41,10 @@ message = "{} of {} jails ({}%) have over 75% data populated post march 10th."
 print(message.format(len(jails_to_include),len(ndata_per_jail), percent))
 
 
-# ## Jails with the last date needs to be within 3 months
-
-# In[12]:
-
+## Jails with the last date needs to be within 3 months
 
 today = pd.to_datetime(datetime.datetime.now().strftime('%m-%d-%Y')) 
 three_months_ago = today - datetime.timedelta(days = 90)
-
-
-# In[13]:
 
 
 jails_most_recent = df.groupby('Facility_Identifier')['Scrape_Date'].max()
@@ -58,10 +52,7 @@ jails_to_include = jails_most_recent[jails_most_recent>=three_months_ago].index
 df = df[df['Facility_Identifier'].isin(jails_to_include)] #Subset master dataset
 
 
-# ## Quantifying Drop Off Again: Generally have seen that all jails that have 75% of data populated also include data from within 3 months:
-
-
-
+## Quantifying Drop Off Again: Generally have seen that all jails that have 75% of data populated also include data from within 3 months:
 within_3_months = len(df)
 within_3_months / over_75percent_data
 
@@ -69,46 +60,28 @@ within_3_months / over_75percent_data
 df['Month'] = df.Scrape_Date.dt.strftime('%m')
 df['Year'] = df.Scrape_Date.dt.strftime('%Y')
 df['DayOfWeek'] = df.Scrape_Date.dt.strftime('%A')
+#Convert Datetime to Strict Date
+df['Scrape_Date'] = df['Scrape_Date'].dt.date
 
-# ## Summarizing Daily Jail Populations
-
+## Summarizing Daily Jail Populations
 
 jail_totals = df.groupby('Scrape_Date')['Population'].agg(['count', 'sum'])
-
-
 jail_totals.columns = ['Count_Of_Jails','Total_Jail_Population']
 jail_totals['Seven_Day_Rolling_Average'] = jail_totals['Total_Jail_Population'].rolling(7).mean()
 
 
-# ## TBD: programatically create list of dates based on most recent weekday (or potentially specific day of week) for each month
-
-# ## First Day of Each Month
-
-# In[64]:
-
-
+## First Day Available of Each Month
 first_days = df.groupby(['Facility_Identifier', 'Month', 'Year']).head(1)
 first_days['Scrape_Date'] = "FirstDay" + first_days['Month'] + "-" + first_days['Year']
 
 
-# ## First Monday of Each Month
-
-# In[19]:
-
-# In[57]:
-
-
+## First Monday of Each Month
 
 mondays = df[df.DayOfWeek=='Monday']
 first_mondays = mondays.groupby(['Facility_Identifier', 'Month', 'Year']).head(1)
 
 
-# In[58]:
-
-
 most_recent = df.groupby(['Facility_Identifier']).tail(1)
-
-# In[27]:
 
 
 ## Verify DataFrame Correctly Sorted by Facility/Date
@@ -117,37 +90,22 @@ assert(all(df == df.sort_values(by=['Facility_Identifier', 'Scrape_Date'])))
 assert(len(most_recent) == df.Facility_Identifier.nunique())
 
 
-# ## March 10th
-
-# In[59]:
-
+## March 10th
 
 march_10 = df[df.Scrape_Date == pd.to_datetime("03-10-2020")]
 
-# ## First Mondays Summary
-
-# In[68]:
-
-
+## First Mondays Summary
 first_monday_summary = first_mondays.groupby(['Facility_Identifier', 'Month', 'Year']).head(1)
-first_monday_summary['Scrape_Date'] = "FirstMonday" + first_monday_summary['Month'] + "-" + first_monday_summary['Year']
+first_monday_summary['Scrape_Date'] = "FirstMondayAvailable" + first_monday_summary['Month'] + "-" + first_monday_summary['Year']
 
 # Stitching Together the Final Summary
-
-
 pre_pivot = pd.concat([march_10, first_mondays, most_recent, first_monday_summary, first_days], ignore_index=True)
-
+# Remove Potential Duplicates (First Mondays and Most Recent could include duplicates)
+pre_pivot = pre_pivot.drop_duplicates(subset=['Facility_Identifier', 'Scrape_Date'])
 final = pre_pivot.pivot(index="Facility_Identifier", columns="Scrape_Date", values="Population")
 
 
-
-
-
-
-# ## Save Transformed Output to Excel
-
-# In[73]:
-
+## Save Transformed Output to Excel
 
 today = datetime.datetime.now().strftime('%m-%d-%Y') #Create string of today's date
 
@@ -155,6 +113,18 @@ filename = "Jail_Summaries_as_of_{}.xlsx".format(today)
 print('Saving to: {}'.format(filename))
 
 with pd.ExcelWriter(filename) as writer1:
-    final.to_excel(writer1, sheet_name = 'FirstMondays')
-    jail_totals.to_excel(writer1, sheet_name = 'DailyJailTotals')
+    to_save = [(final, 'FirstMondays'),(jail_totals, 'DailyJailTotals')]
+    #final.to_excel(writer1, sheet_name = 'FirstMondays')
+    #jail_totals.to_excel(writer1, sheet_name = 'DailyJailTotals')
+    #Fix Column Widths
+    for df, sheetname in to_save:
+        df.to_excel(writer1, sheet_name = sheetname)
+        worksheet = writer1.sheets[sheetname]  # pull worksheet object
+        for idx, col in enumerate(df):  # loop through all columns
+            series = df[col]
+            max_len = max((
+                series.astype(str).map(len).max(),  # len of largest item
+                len(str(series.name))  # len of column name/header
+                )) + 12  # adding a little extra space
+            worksheet.set_column(idx, idx, max_len)  # set column width
 
